feb24ea6b31bcdb6d361fbaa7445f0770c8145db
Isac Galdino Ferreira <sougoku705@gmail.com>
2026-09-04 11:23:13 +0000

    teste2 pra saber se essa bagaça vai dar certo

---
+++ teste2.py
@@ -0,0 +1,12 @@
+class familia-galdino:
+    def __init__(self, nome-pai="nome nao informado", nome-mae="nome nao informado", nome-irma1="nome nao informado", nome-irma2="nome nao informado", nome-irmao="nome nao informado")
+    self.nome-pai= nome-pai
+    self.nome-mae= nome-mae
+    self.nome-irma1= nome-irma1
+    self.nome-irma2= nome-irma2
+    self.nome-irmao= nome-
+    def detalhes(self):
+        return f"O nome do seu pai é {self.nome-pai}, o nome da sua mãe é {self.nome-mae}, o nome da sua irmã mais velha é {self.nome-irma1} e o nome da sua irmã mais nova é {self.nome-irma2} e o nome do seu único irmão é {self.nome-irmao}"
+
+nomes-familia-galdino = nomes("ferreira","rosa","ingrid","dora","italo")
+print(nomes-familia-galdino.nomes())
