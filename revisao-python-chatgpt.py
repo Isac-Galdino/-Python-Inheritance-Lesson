71d60e1fcaed7005ce74a7a8e664b7d104090387
Isac Galdino Ferreira <sougoku705@gmail.com>
2026-09-17 13:08:24 +0000

    alterações feitas para a criação de um sistema de médias de alunos

--- revisao-python-chatgpt.py
+++ revisao-python-chatgpt.py
@@ -1,21 +1,30 @@
 class aluno:
-    def __init__(self, nome, idade, curso, media):
+    def __init__(self, nome, idade, curso, media, email):
         self.nome = nome
         self.idade = idade
         self.curso = curso
-        self.__media = media
+        if media >= 0 and media <= 10:
+            self.__media = media
+        else:
+            raise ValueError("A média informada é inválida. Informe uma média válida")
+
+        if "@" not in email:
+            raise ValueError("O email informado é inválido")
+        else:
+            self.__email = email
 
     def apresentar(self):
         return f"Nome: {self.nome}\nIdade: {self.idade} anos\nCurso: {self.curso}\nMedia: {self.__media}"
 
     def get_media(self):
-        return f"A nota deste aluno é {self.__media}"
-
+        return f"A média deste aluno é {self.__media}"
+    
     def set_media(self, nova_media):
         if nova_media >= 0 and nova_media <= 10:
             self.__media = nova_media
+            return("Média alterada com sucesso!!")
         else:
-            return f"A nota informada não é válida"
+            return f"A média não pode ser menor que 0 e maior que 10. Informe uma média válida"
 
     def verificar_aprovacao(self):
         if self.__media >= 6:
@@ -24,11 +33,12 @@ class aluno:
             return f"Tente melhor na próxima vez, você consegue {self.nome}"
         else:
             return f"{self.nome}, infelizmente você está reprovado(a)"
-
-aluno1 = aluno("galdino", 16, "Desenvolvimento de sistemas", 6)
+        
+aluno1 = aluno("Isac Galdino Ferreira", 16, "Desenvolvimento de Sistemas", 8, "isac.galdino.gmail.com")
+print(aluno1.set_media(9))
 print(aluno1.get_media())
-print(aluno1.set_media(8))
-print(aluno1.apresentar())
+print(aluno1.verificar_aprovacao())
+      
 
             
     
--- revisao-python.py
+++ revisao-python.py
@@ -84,4 +84,62 @@ class game:
 
 game1 = game("God of war", 100)
 game1.valor_acrescentado(0)
-print(game1.mostrar_resultado())            
+print(game1.mostrar_resultado())
+#projeto de recebimento de notas de alunos e ajuste de notas
+class aluno:
+    def __init__(self, nome, idade, curso, media, email):
+        self.nome = nome
+        self.idade = idade
+        self.curso = curso
+        if media >= 0 and media <= 10:
+            self.__media = media
+        else:
+            raise ValueError("A média informada é inválida. Informe uma média válida")
+
+        if "@" not in email:
+            raise ValueError("O email informado é inválido")
+        else:
+            self.__email = email
+
+    def apresentar(self):
+        return f"Nome: {self.nome}\nIdade: {self.idade} anos\nCurso: {self.curso}\nMedia: {self.__media}"
+
+    def get_media(self):
+        return f"A média deste aluno é {self.__media}"
+    
+    def set_media(self, nova_media):
+        if nova_media >= 0 and nova_media <= 10:
+            self.__media = nova_media
+            return("Média alterada com sucesso!!")
+        else:
+            return f"A média não pode ser menor que 0 e maior que 10. Informe uma média válida"
+
+    def verificar_aprovacao(self):
+        if self.__media >= 6:
+            return f"PARABÉNS {self.nome}!!! Você está aprovado(a)"
+        elif self.__media < 6 and self.__media >= 4:
+            return f"Tente melhor na próxima vez, você consegue {self.nome}"
+        else:
+            return f"{self.nome}, infelizmente você está reprovado(a)"
+        
+aluno1 = aluno("Isac Galdino Ferreira", 16, "Desenvolvimento de Sistemas", 8, "isac.galdino.gmail.com")
+print(aluno1.set_media(9))
+print(aluno1.get_media())
+print(aluno1.verificar_aprovacao())
+
+#novo projeto: Desenvolvimento de sistemas de entregas ou sistema de loja online
+class Loja:
+    def __init__(self, emitent, destin, nota_ident):
+        self.emitent = emitent
+        self.destin = destin
+        if nota_ident > 44 and nota_ident < 44:
+            self.__nota_ident = nota_ident
+        else:
+            raise ValueError("O número de nota fiscal informada é inválida")
+
+    def get_info(self):
+        
+        
+
+            
+    
